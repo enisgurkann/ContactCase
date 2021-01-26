440e185026508c5768696eb5447c3c243024e31a
Enis <enes_gurkan@hotmail.com>
2021-01-26 15:23:55 +0300

    conf

---
+++ ContactCase.ContactApi/Data/ContactConf.cs
@@ -0,0 +1,21 @@
+﻿using ContactCase.ContactApi.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactCase.ContactApi.Data
+{
+    public class ContactConf : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.HasKey(entity => entity.Id);
+            builder.Property(entity => entity.FirstName).HasMaxLength(100);
+            builder.Property(entity => entity.LastName).HasMaxLength(100);
+            builder.Property(entity => entity.CompanyName).HasMaxLength(250);
+        }
+    }
+}
