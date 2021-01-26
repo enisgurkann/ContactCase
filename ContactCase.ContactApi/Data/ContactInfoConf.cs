176d27c8fae4ce89a31e7484cf42ab98b7c82423
Enis <enes_gurkan@hotmail.com>
2021-01-26 15:25:23 +0300

    conf2

---
+++ ContactCase.ContactApi/Data/ContactInfoConf.cs
@@ -0,0 +1,20 @@
+﻿using ContactCase.ContactApi.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactCase.ContactApi.Data
+{
+    public class ContactInfoConf : IEntityTypeConfiguration<ContactInfo>
+    {
+        public void Configure(EntityTypeBuilder<ContactInfo> builder)
+        {
+            builder.HasKey(entity => entity.Id);
+            builder.Property(entity => entity.InfoType).HasMaxLength(100);
+            builder.Property(entity => entity.Value).HasMaxLength(150);
+        }
+    }
+}
