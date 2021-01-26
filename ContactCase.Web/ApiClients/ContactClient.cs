015a0bf710e041221a797ef5d3fb0113177b4397
Enis <enes_gurkan@hotmail.com>
2021-01-26 23:25:12 +0300

    contact commşt

--- ContactCase.Web/ApiClients/ContactClient.cs
+++ ContactCase.Web/ApiClients/ContactClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContactCase.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -13,31 +14,31 @@ namespace ContactCase.Web.ApiClients
 
         public ContactClient(HttpClient httpClient) => _httpClient = httpClient;
 
-        public Task<ContactListViewModel> GetContacts(int pageIndex, int pageSize)
+        public Task<List<ContactModel>> GetContacts(int pageIndex, int pageSize)
         {
-            var response = _httpClient.GetFromJsonAsync<ContactListViewModel>($"api/contacts?pageIndex={pageIndex}&pageSize={pageSize}");
+            var response = _httpClient.GetFromJsonAsync<List<ContactModel>>($"api/contacts?pageIndex={pageIndex}&pageSize={pageSize}");
             return response;
         }
 
-        public async Task<ContactViewModel> GetById(Guid id)
+        public async Task<ContactModel> GetById(Guid id)
         {
             var response = await _httpClient.GetAsync($"api/v1/contacts/{id}");
             if (response.IsSuccessStatusCode)
-                return await response.Content.ReadFromJsonAsync<ContactViewModel>();
+                return await response.Content.ReadFromJsonAsync<ContactModel>();
 
             return null;
         }
 
-        public async Task<ContactViewModel> Create(ContactViewModel model)
+        public async Task<ContactModel> Create(ContactModel model)
         {
             var response = await _httpClient.PostAsJsonAsync("api/v1/contacts", model);
-            return await response.Content.ReadFromJsonAsync<ContactViewModel>();
+            return await response.Content.ReadFromJsonAsync<ContactModel>();
         }
 
-        public async Task<ContactViewModel> Updatet(ContactViewModel model)
+        public async Task<ContactModel> Updatet(ContactModel model)
         {
             var response = await _httpClient.PutAsJsonAsync("api/v1/contacts", model);
-            return await response.Content.ReadFromJsonAsync<ContactViewModel>();
+            return await response.Content.ReadFromJsonAsync<ContactModel>();
         }
 
         public async Task<bool> Delete(Guid id)
