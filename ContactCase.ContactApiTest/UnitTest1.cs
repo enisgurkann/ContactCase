7f94bb8a5f64806895edfc315a1a3e16b81dfbbe
Enis <enes_gurkan@hotmail.com>
2021-01-27 23:34:02 +0300

    Test

---
+++ ContactCase.ContactApiTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ContactCase.ContactApiTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Create()
+        {
+
+        }
+    }
+}
